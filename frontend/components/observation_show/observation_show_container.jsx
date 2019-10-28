151daaae91352df140feff066c7e5fba9d7c365d
Fue <fher.209@gmail.com>
2019-10-28 11:59:24 -0700

    Add identification and comments state to props on observation show container

--- frontend/components/observation_show/observation_show_container.jsx
+++ frontend/components/observation_show/observation_show_container.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchObservation, deleteObservation } from '../../actions/observations_actions';
+import { fetchIdentifications, deleteIdentification } from '../../actions/identifications_actions';
+import { fetchComments, deleteComment } from '../../actions/comments_actions';
 import { Link } from 'react-router-dom';
 
 import ObservationShow from './observation_show';
 
-const mapSTP = ({session, entities: {observations: {observation}} }) => ({
+const mapSTP = ({session, entities: {observations: {observation}, identifications, comments} }) => ({
   observation,
-  currentUser: session.id
+  currentUser: session.id,
+  identifications: Object.values(identifications).filter(identification => identification.observation_id === observation.id),
+  comments: Object.values(comments).filter(comment => comment.observation_id === observation.id)
 })
 
 
 const mapDTP = dispatch => ({
   fetchObservation: id => dispatch(fetchObservation(id)),
   deleteObservation: id => dispatch(deleteObservation(id)),
+  fetchIdentifications: () => dispatch(fetchIdentifications()),
+  deleteIdentification: id => dispatch(deleteIdentification(id)),
+  fetchComments: () => dispatch(fetchComments()),
+  deleteComment: id => dispatch(deleteComment(id))
 })
 
 export default connect(
