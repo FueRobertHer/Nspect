197acfd80c8b0f8ce099bb54096cdfd8253c0f9c
Fue <fher.209@gmail.com>
2020-08-25 19:19:52 -0700

    Start refactoring to functional component

--- frontend/components/edit_observation/edit_location_map.jsx
+++ frontend/components/edit_observation/edit_location_map.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { withRouter } from 'react-router-dom';
-
 import MarkerManager from '../../util/marker_manager';
 
 const getCoordsObj = latLng => ({
--- frontend/components/edit_observation/edit_observation_container.jsx
+++ frontend/components/edit_observation/edit_observation_container.jsx
@@ -1,15 +1,14 @@
-import React from 'react';
 import { connect } from 'react-redux';
 import { fetchObservation, updateObservation } from '../../actions/observations_actions';
-import { Link } from 'react-router-dom';
 
 import EditObservationForm from './edit_observation_form';
 
-const mapSTP = ({entities: {observations: {observation}} }) => {
-  return ({
-    observation,
-  })
-};
+const mapSTP = ({ session, entities: { users, observations } }, { match }) => ({
+  userId: session.id,
+  currentUser: users[session.id],
+  observationId: match.params.observation_id,
+  observation: observations[match.params.observation_id]
+});
 
 
 const mapDTP = dispatch => ({
